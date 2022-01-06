ef39647087a5e1bc506f03fd960b5b8c6e738099
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2022-01-06 00:07:56 -0600

    Portfolio Updates
    Updated the following:
    - Actual images, image styling, image movement and position relative to browser resizing
    - Minor color updates (i.e., dots, etc.)
    - Overall aesthetics
    - Attempted to eliminate any code that is unnecessary using DevTools
    - Revised some of the verbiage with the intention of refining my language, etc.

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -26,8 +26,8 @@ function showSlides(n) {
     var dots = document.getElementsByClassName("dot"); // This stores all elements name "dot" in the variable array "dots"
     if (n > slides.length) {slideIndex = 1}; // If n (the number passed into the function) is greater than the length of the array "slides", the slideIndex is set to 1
     if (n < 1) {slideIndex = slides.length}; // If n (the number passed into the function) is less than 1, te slideIndex is set to the length of the array "slides"
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; // Loop takes items in array "slides" and sets the display to none
+    for (i = 0; i < slides.length; i++) { // Loop takes items in array "slides" and sets the display to none
+        slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", ""); // Loop loop takes each item in the array "dots" and removes "active" (active styling)
